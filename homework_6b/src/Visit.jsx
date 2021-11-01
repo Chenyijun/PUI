01789d10d519211e82a43755329d843d637a92ad
Annie <anniechen0502@gmail.com>
2021-11-01 11:44:14 -0400

    Updated missing pages/package

---
+++ homework_6b/src/Visit.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+const Visit = () => {
+  return (
+    <div className="content" id="centerColumn">
+		<h1>Visit</h1>
+		<div id="twoColGrid">
+			<div>
+				<p>Come visit us at our physical location: <br/> 1234 Bun Drive Pittsburgh, PA 15213</p>
+				<p>Our hours: <br/> Monday - Friday 7am - 2pm</p>
+			</div>
+			<img src="fakeMap.png" id="map" alt="Map of store"/>
+		</div>
+	</div>
+  )
+}
+
+export default Visit
